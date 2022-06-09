55ca83b55cc9e63e54103f47341f80516639c1df
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2022-06-09 16:41:44 +0530

    Time: 3 ms (79.21%), Space: 7.6 MB (91.98%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,28 +1,22 @@
 class Solution {
-public:
-     void per(vector<vector<int>> &ans, vector<int> &temp,int n,bool mp[],vector<int>& nums)
-     {
-             if(temp.size()==n) 
-        { ans.push_back(temp);
-         return;
-         }
-         for(int i=0;i<n;i++)
-         {
-             if(!mp[i])
-             { temp.push_back(nums[i]);
-             mp[i]=1;
-             per(ans,temp,n,mp,nums);
-             mp[i]=0;
-             temp.pop_back();   }
-         }
-     }
+public: 
+void per(int i,vector<vector<int>> &ans,vector<int> &nums)
+{ if(i==nums.size())
+{
+    ans.push_back(nums);
+    return;
+}
+ for(int j=i;j<nums.size();j++)
+ {
+     swap(nums[i],nums[j]);
+     per(i+1,ans,nums);
+     swap(nums[i],nums[j]);
+ }
+    
+}
     vector<vector<int>> permute(vector<int>& nums) {
-         int n=nums.size();
-         vector<vector<int>>ans;
-        vector<int> temp;
-        bool mp[n];
-        for(int j=0;j<n;j++) mp[j]=0;
-        per(ans,temp,n,mp,nums);
-        return ans;
+    vector<vector<int>> ans;
+        per(0,ans,nums);
+    return ans;
     }
 };
