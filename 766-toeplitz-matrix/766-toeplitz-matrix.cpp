ccaba10cc6e82e3e442c4d7f63694415f102e090
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2022-08-08 00:56:15 +0530

    Time: 24 ms (53.19%), Space: 17.5 MB (17.61%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.cpp
+++ 766-toeplitz-matrix/766-toeplitz-matrix.cpp
@@ -2,18 +2,12 @@ class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
         int r=matrix.size();
-map<int,int> mp;
         int c=matrix[0].size();
-        for(int i=0;i<r;i++)
+        for(int i=1;i<r;i++)
         {
-            for(int j=0;j<c;j++)
+            for(int j=1;j<c;j++)
             {
-                if(mp.find(i-j)==mp.end())
-                mp[i-j]=matrix[i][j];
-                else
-                {
-                    if(mp[i-j] !=matrix[i][j]) return false;
-                }
+             if(matrix[i][j]!=matrix[i-1][j-1]) return false; 
             }
         }
     return true;
