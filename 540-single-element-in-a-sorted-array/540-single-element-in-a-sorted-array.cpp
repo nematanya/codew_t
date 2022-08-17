10d645c94b3cd415dd51ba4b63f6adab4f5e5dac
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2022-08-17 23:48:11 +0530

    Time: 63 ms (8.63%), Space: 22.4 MB (55.79%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int x=0;
-        for(auto it: nums)
-            x=x^it;
-        return x;
+        int s=0,e=nums.size()-2;
+        int mid;
+        while(s<=e)
+        { mid=s+(e-s)/2;
+            if(mid%2==0)
+            {
+                if(nums[mid+1]!=nums[mid])
+                    e=mid-1;
+                else 
+                    s=mid+1;
+            }
+            else{
+                if(nums[mid-1]!=nums[mid])
+                    e=mid-1;
+                else s=mid+1;
+            }
+        }
+        return nums[s];
     }
 };
