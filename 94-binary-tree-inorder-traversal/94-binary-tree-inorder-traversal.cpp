ddfb230584a0fb134751eba986ccea9ca3b623e9
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2022-08-18 10:32:46 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (75.20%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,19 +9,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//morris traversal
 class Solution {
 public:
-    void sol(TreeNode* root,vector<int>& ans)
-    {
-        if(!root)
-            return;
-        sol(root->left,ans);
-        ans.push_back(root->val);
-        sol(root->right,ans);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        sol(root,ans);
-        return ans;
+        TreeNode* curr=root;
+            vector<int> v;
+        while(curr)
+        {
+            if(curr->left==NULL)
+            {
+                v.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode* temp=curr->left;
+                while(temp->right and temp->right!=curr)
+                temp=temp->right;
+                if(temp->right==NULL){
+                    temp->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    temp->right=NULL;
+                    v.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+        return v;
     }
 };
