fdd7d86f3e361b3ff7b537d5cf1ace9b0ba2f50b
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2022-08-28 01:20:53 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (95.27%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -11,15 +11,23 @@ public:
     int rob(vector<int>& nums) {
         
         int n=nums.size();
-        vector<int> dp(n,-1);
-        dp[0]=nums[0];
+        // vector<int> dp(n,-1);
+        int prev=nums[0];
+       int prev2=0;
+    
         for(int i=1;i<n;i++)
         {
+            // int pick=nums[i];
+            // if(i>1) pick+=dp[i-2];
+            // int non=dp[i-1];
+            // dp[i]=max(pick,non);
             int pick=nums[i];
-            if(i>1) pick+=dp[i-2];
-            int non=dp[i-1];
-            dp[i]=max(pick,non);
+            if(i>1) pick+=prev2;
+            int non=prev;
+            int curr=max(pick,non);
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n-1];
+        return prev;
     }
 };
