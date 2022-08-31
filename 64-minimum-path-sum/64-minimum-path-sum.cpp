33e42828716b55bbf1867ddc052db64db7c22c16
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2022-08-31 16:42:13 +0530

    Time: 21 ms (23.89%), Space: 10.1 MB (55.90%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -12,6 +12,21 @@ public:
         int r=grid.size();
         int c=grid[0].size();
 vector<vector<int>>dp(r,vector<int>(c,-1));
-        return sol(r-1,c-1,grid,dp);
+
+        // return sol(r-1,c-1,grid,dp);
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {  
+                if(i==0 and j==0) dp[i][j]=grid[i][j];
+                else{
+          int up=INT_MAX,left=INT_MAX;
+                 if(i>0)up=grid[i][j]+dp[i-1][j];
+                 if(j>0)left=grid[i][j]+dp[i][j-1];
+                    dp[i][j]=min(up,left);
+                }
+            }
+        }
+        return dp[r-1][c-1];
         }
 };
