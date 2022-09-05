52ce3aa648a31e20500a23d28cc145c5cc398b5a
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2022-09-05 11:32:30 +0530

    Time: 8 ms (100.00%), Space: 8.6 MB (100.00%) - LeetHub

---
+++ 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int f(vector<vector<int>>& mat, string t)
+    {int r=mat.size();
+     int ans=0;
+    int c=mat[0].size();
+     for(int i=0;i<r;i++)
+     {
+         for(int j=0;j<c;j++)
+         {
+             if(t[j]=='0' and mat[i][j]==1  )
+                 break;
+             if(j==c-1) 
+                 ans++;
+         }
+     }
+     return ans;
+    }
+    int maximumRows(vector<vector<int>>& mat, int cols) {
+        int m=mat[0].size();
+       string t(m,'1');
+        for(int i=0;i<m-cols;i++) 
+            t[i]='0';
+        int res=0;
+        do{
+            res=max(res,f(mat,t));
+        }while(next_permutation(t.begin(),t.end()));
+    return res;
+    }
+    
+};
