a542a6e1cfd5b5cec77d39775bb13ef875ff297e
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2022-09-05 12:51:41 +0530

    Time: 78 ms (96.80%), Space: 24.5 MB (79.20%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     string repeatLimitedString(string s, int repeatLimit) {
-
-vector<int>v(26,0);
-for(auto i:s){
-v[i-'a']++;
-}
-string k;
-for(int i=25;i>=0;i--){
-int count=0;
-while(v[i]!=0){
-if(count==repeatLimit){
-int j=i-1;
-while(j>=0&&v[j]==0){
-j--;
-}
-if(j==-1) break;
-k+='a'+j;
-v[j]--;
-count=0;
-}
-k+='a'+i;
-v[i]--;
-count++;
-}
-}
-return k;
-}
-    
+        vector<int> v(26,0);
+        for(int i=0;i<s.length();i++)
+            v[s[i]-'a']++;
+       string res="";
+        for(int i=25;i>=0;i--)
+        {
+         int count=0;
+            while(v[i]!=0)
+            {
+                if(count==repeatLimit)
+                {
+                    int j=i-1;
+                    while(j>=0 and v[j]==0)
+                        j--;
+                    if(j==-1) break;
+                    else{ res+=('a'+j);
+                    v[j]--;
+                    count=0;}
+                }
+                
+                res+=('a'+i);
+                    v[i]--;
+                    count++;}
+            }
+        
+        
+        return res;
+    }
 };
