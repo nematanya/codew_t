e256a8b6e92dfcd6955fcfa9d2a7e3ab1c7f6e4e
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2022-09-20 19:38:50 +0530

    Time: 1838 ms (5.01%), Space: 206.9 MB (40.01%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -24,15 +24,12 @@ public:
         // }
         for(int i=n-1;i>=0;i--)
         {
-            for(int buy=0;buy<=1;buy++)
-            {
                 for(int cap=1;cap<=2;cap++)
-                { if(buy)
-                    dp[i][buy][cap]=max(-prices[i]+dp[i+1][0][cap],dp[i+1][1][cap]);
-                 else
-                    dp[i][buy][cap]=max(prices[i]+dp[i+1][1][cap-1],dp[i+1][0][cap]);
-                }
-            }
+                { 
+                    dp[i][1][cap]=max(-prices[i]+dp[i+1][0][cap],dp[i+1][1][cap]);
+                
+                    dp[i][0][cap]=max(prices[i]+dp[i+1][1][cap-1],dp[i+1][0][cap]);
+                } 
         }
      return dp[0][1][2];
     }
