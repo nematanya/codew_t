c5727f978497c097e5586e3d8caa483b0e2a9545
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2022-09-21 12:08:56 +0530

    Time: 873 ms (35.68%), Space: 144.6 MB (41.32%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
-    // int sol(int i,vector<int>& prices,int buy,int cap,vector<vector<vector<int>>> &dp)
-    // { if(cap==0) return 0;
-    //     if(i==prices.size()) return 0;
-    //  if(dp[i][buy][cap]!=-1) return dp[i][buy][cap];
-    //  if(buy)
-    //      return dp[i][buy][cap]=max(-prices[i]+sol(i+1,prices,0,cap,dp),0+sol(i+1,prices,1,cap,dp));
-    //  else
-    //      return dp[i][buy][cap]=max(prices[i]+sol(i+1,prices,1,cap-1,dp),0+sol(i+1,prices,0,cap,dp));
-    // }
+    int sol(int i,vector<int>& prices, int t,vector<vector<int>>&dp)
+    {
+        if(i==prices.size()) return 0;
+        if(t==4) return 0;
+        if(dp[i][t]!=-1)return dp[i][t];
+        if(t&1)
+            return dp[i][t]=max(prices[i]+sol(i+1,prices,t+1,dp),sol(i+1,prices,t,dp));
+        else
+            return dp[i][t]=max(-prices[i]+sol(i+1,prices,t+1,dp),sol(i+1,prices,t,dp));
+        
+    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int b=0;b<2;b++)
-        //         dp[i][b][0]=0;
-        // }
-        // for(int b=0;b<2;b++)
-        // {
-        //     for(int c=0;b<3;c++)
-        //         dp[n][b][c]=0;
-        // }
-        for(int i=n-1;i>=0;i--)
-        {
-                for(int cap=1;cap<=2;cap++)
-                { 
-                    dp[i][1][cap]=max(-prices[i]+dp[i+1][0][cap],dp[i+1][1][cap]);
-                
-                    dp[i][0][cap]=max(prices[i]+dp[i+1][1][cap-1],dp[i+1][0][cap]);
-                } 
-        }
-     return dp[0][1][2];
+    vector<vector<int>>dp(n+1,vector<int>(5,-1));
+        return sol(0,prices,0,dp);
     }
 };
