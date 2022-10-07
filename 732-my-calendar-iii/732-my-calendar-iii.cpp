d40114a3e06a532bf92d868ed35826e44a9207bf
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2022-10-08 00:14:31 +0530

    Time: 298 ms (20.11%), Space: 26.4 MB (75.61%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -4,9 +4,9 @@ public:
     int book(int s, int e) {
         times[s]++; 
         times[e]--; 
-        int events_on = 0, ans = 0;
+        int n = 0, ans = 0;
         for (pair<int, int> x : times)
-            ans = max(ans, events_on += x.second);
+            ans = max(ans, n += x.second);
         return ans;
     }
 };
