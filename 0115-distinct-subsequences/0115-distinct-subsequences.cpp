31e67a03f97743fa8e1a496ecd9daefcdd70beb6
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2023-01-30 19:15:22 +0530

    Time: 43 ms (72.93%), Space: 30.1 MB (34.12%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    // int sol(int i,int j,string s, string t,vector<vector<int>> &dp)
+    // {
+    //     if(j<0) return 1;
+    //     if(i<0) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(s[i]==t[j])
+    //     {
+    //         return dp[i][j]=sol(i-1,j-1,s,t,dp)+sol(i-1,j,s,t,dp);
+    //     }
+    //     else
+    //         return dp[i][j]=sol(i-1,j,s,t,dp);
+    // }
+    int numDistinct(string s, string t) {
+        int n=s.size(),m=t.size();
+       vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+        for(int i=0;i<=n;i++)
+            dp[i][0]=1;
+     for(int i=1;i<=n;i++)
+     {
+         for(int j=1;j<=m;j++)
+         {
+             if(s[i-1]==t[j-1])
+                 dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+             else
+                 dp[i][j]=dp[i-1][j];
+         }
+     }
+         
+
+        return dp[n][m];
+    }
+};
