30605f8a70445cc76680ebbde4509f22f91c6056
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2023-01-31 11:10:11 +0530

    Time: 18 ms (54.38%), Space: 9 MB (41.52%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    int sol(int i,int j,string s,string t, vector<vector<int>>&dp)
-    {
-        if(i<0) return j+1;
-        if(j<0) return i+1;if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]) return dp[i][j]= sol(i-1,j-1,s,t,dp);
-        else
-            return dp[i][j]=1+min(sol(i,j-1,s,t,dp),min(sol(i-1,j,s,t,dp),sol(i-1,j-1,s,t,dp)));
-    }
+    // int sol(int i,int j,string s,string t, vector<vector<int>>&dp)
+    // {
+    //     if(i<0) return j+1;
+    //     if(j<0) return i+1;if(dp[i][j]!=-1) return dp[i][j];
+    //     if(s[i]==t[j]) return dp[i][j]= sol(i-1,j-1,s,t,dp);
+    //     else
+    //         return dp[i][j]=1+min(sol(i,j-1,s,t,dp),min(sol(i-1,j,s,t,dp),sol(i-1,j-1,s,t,dp)));
+    // }
     int minDistance(string s, string t) {
         int n=s.size(),m=t.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-       return sol(n-1,m-1,s,t,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=m;i++)
+            dp[0][i]=i;
+for(int i=1;i<=n;i++)
+    dp[i][0]=i;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+                  if(s[i-1]==t[j-1])  dp[i][j]= dp[i-1][j-1];
+        else
+            dp[i][j]=1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+        }
+       return dp[n][m];
         
     }
 };
