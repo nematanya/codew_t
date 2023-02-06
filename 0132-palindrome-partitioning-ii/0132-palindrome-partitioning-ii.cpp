c9759c707f0db22f2d81b7e710b72733b2e8b287
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2023-02-06 16:52:16 +0530

    Time: 463 ms (38.60%), Space: 6.6 MB (66.12%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -9,39 +9,25 @@ public:
         }
         return true;
     }
-//     int sol(int i,int n, string &s,vector<int>& dp)
-//     {
-//         if(i==n) return 0;
-    
-//         if(dp[i]!=-1)
-//             return dp[i];
-//         for(int j=i;j<n;j++)
-//         {
-//             if(ispal(i,j,s))
-//             {
-//                 int cost=1+sol(j+1,n,s,dp);
-//                 mini=min(mini,cost);
-//             }
-//         }
-//         return dp[i]=mini;
-//     }
-    int minCut(string s) {
-        int n=s.size();
-        vector<int>dp(n+1,0);
-        dp[n]=0;
-        for(int i=n-1;i>=0;i--)
-        {        int mini=INT_MAX;
-
-               for(int j=i;j<n;j++)
+    int sol(int i,int n, string &s,vector<int>& dp)
+    {
+        if(i==n) return 0;
+        int mini=INT_MAX;
+        if(dp[i]!=-1)
+            return dp[i];
+        for(int j=i;j<n;j++)
         {
             if(ispal(i,j,s))
             {
-                int cost=1+dp[j+1];
+                int cost=1+sol(j+1,n,s,dp);
                 mini=min(mini,cost);
             }
         }
-            dp[i]=mini;
-        }
-      return dp[0]-1;  
+        return dp[i]=mini;
+    }
+    int minCut(string s) {
+        int n=s.size();
+        vector<int>dp(n,-1);
+      return sol(0,n,s,dp)-1;  
     }
 };
