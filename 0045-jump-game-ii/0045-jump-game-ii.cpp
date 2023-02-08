875e62f3acb17866d74894ac40cc2dfd5f8d5097
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2023-02-08 12:08:54 +0530

    Time: 489 ms (22.96%), Space: 17.3 MB (29.40%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -10,7 +10,14 @@ public:
         return dp[i]= ans;
     }
     int jump(vector<int>& nums) {
+        int n=nums.size();
         vector<int>dp(nums.size(),10004);
-        return sol(nums,0,dp);
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=1;j<=nums[i];j++)
+                dp[i]=min(dp[i],1+dp[min(n-1,i+j)]);
+        }
+        return dp[0];
     }
 };
