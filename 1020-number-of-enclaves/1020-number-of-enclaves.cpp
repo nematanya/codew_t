d3e306b255467757fe5c18195b602bb51b483d6a
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2023-02-14 15:34:20 +0530

    Time: 92 ms (35.19%), Space: 34 MB (18.09%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+        void dfs(int i,int j,vector<vector<int>>&vis,vector<vector<int>>& board,vector<int> &dx, vector<int>&dy)
+    {
+        int n=board.size();
+        int m=board[0].size();
+        vis[i][j]=1;
+        for(int t=0;t<4;t++)
+        {
+            int row=i+dx[t];
+            int col=j+dy[t];
+            if(row>=0 and row<n and col>=0 and col<m and !vis[row][col] and board[row][col]==1)
+            {
+                dfs(row,col,vis,board,dx,dy);
+            }
+        }
+        }
+    int numEnclaves(vector<vector<int>>& board) {
+                int n=board.size();
+        int m=board[0].size();
+        vector<int>dx={-1,0,1,0};
+        vector<int>dy={0,1,0,-1};
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i][0] and board[i][0]==1)
+                dfs(i,0,vis,board,dx,dy);
+                if(!vis[i][m-1] and board[i][m-1]==1)
+                    dfs(i,m-1,vis,board,dx,dy);
+            
+        }
+        for(int i=0;i<m;i++)
+        {
+                        if(!vis[0][i] and board[0][i]==1)
+                dfs(0,i,vis,board,dx,dy);
+                if(!vis[n-1][i] and board[n-1][i]==1)
+                    dfs(n-1,i,vis,board,dx,dy);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(vis[i][j]!=1 and board[i][j]==1)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
