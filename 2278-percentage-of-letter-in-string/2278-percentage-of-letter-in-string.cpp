a08c77a2739fd2d0aff79e7092d4a3dfb24c2bee
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2023-03-10 16:05:23 +0530

    Time: 5 ms (33.69%), Space: 6.4 MB (12.66%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int percentageLetter(string s, char letter) {
         unordered_map<char,int>m;
-
+float ans;
         for(int i=0;i<s.size();i++)
             m[s[i]]++;
         
-        return (1.0*m[letter]/s.size())*100;
+        cout<<m[letter]/(float)s.length();
+        ans=(1.0*m[letter]/s.size())*100;
+        return ans;
     }
 };
