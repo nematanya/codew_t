3c3a3961d99c234189a47c2f80689a67a2e506e9
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2023-03-18 19:00:53 +0530

    Time: 23 ms (15.64%), Space: 25.9 MB (34.28%) - LeetHub

--- 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,25 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    unordered_map<int,int> m;
 public:
-    TreeNode* util(vector<int>& pre,vector<int>& post,int preStart,int preEnd,int postStart,int postEnd){
-        if(preStart > preEnd)
+    TreeNode* sol(vector<int>&pre,int pres,int pree,vector<int>&post,int posts,int poste, unordered_map<int,int> &mp)
+    {
+        if(pres>pree)
             return NULL;
-        TreeNode* root = new TreeNode(pre[preStart]);
-        if(preStart == preEnd)
-            return root;
-        
-        int idx = m[pre[preStart + 1]];
-        int offset = idx - postStart;
-        root->left = util(pre,post,preStart+1,preStart+1+offset,postStart,idx);
-        root->right = util(pre,post,preStart+1+offset+1,preEnd,idx+1,postEnd);
+        TreeNode* root=new TreeNode(pre[pres]);
+            if(pres==pree)return root;
+        int idx=mp[pre[pres+1]];
+        int dis=idx-posts;
+        root->left=sol(pre,pres+1,pres+dis+1,post,posts,idx,mp);
+        root->right=sol(pre,pres+1+dis+1,pree,post,idx+1,poste,mp);
         return root;
     }
-    
     TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
-        int n = pre.size();
-        for(int i = 0;i < n;i++)
-            m[post[i]] = i;
-        return util(pre,post,0,n-1,0,n-1);
+        unordered_map<int,int>mp;
+        for(int i=0;i<post.size();i++)
+            mp[post[i]]=i;
+        return sol(pre,0,pre.size()-1,post,0,post.size()-1,mp);
     }
 };
