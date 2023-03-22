dee414f5c4b114c180d88d28ffb5db134bf5d638
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2023-03-22 11:31:50 +0530

    Time: 446 ms (87.69%), Space: 123.4 MB (85.85%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    void dfs(vector<pair<int,int>> adjacent[], vector<int>&v, int &ans, int t)
+    void dfs(vector<pair<int,int>> adj[],vector<bool> &v,int &ans,int c)
     {
-        if(v[t]==1) return;
-        v[t]=1;
-        for(auto it:adjacent[t])
+        if(v[c]==1)return;
+        v[c]=1;
+        for(auto it:adj[c])
         {
-            ans = min(ans,it.second);
-            dfs(adjacent,v,ans,it.first);
+            ans=min(ans,it.second);
+            dfs(adj,v,ans,it.first);
         }
         return;
     }
-    int minScore(int n, vector<vector<int>>& roads)
-    {
-        int sizee= roads.size();
-        vector<pair<int,int>>adjacent[n+1];
-        for(int i=0;i<sizee;i++)
-        {
-            int a = roads[i][0];
-            int b = roads[i][1];
-            int c = roads[i][2]; 
-            adjacent[a].push_back({b,c});
-            adjacent[b].push_back({a,c});
-        }
-        vector<int>visited(n+1,0);
+    int minScore(int n, vector<vector<int>>& roads) {
+        vector<pair<int,int>>adj[n+1];
+        vector<bool>vis(n+1,0);
         int ans=INT_MAX;
-        dfs(adjacent,visited,ans,1);
+        for(int i=0;i<roads.size();i++)
+        {
+            int a=roads[i][0];
+            int b=roads[i][1];
+            int c=roads[i][2];
+            adj[a].push_back({b,c});
+            adj[b].push_back({a,c});
+        }
+        dfs(adj,vis,ans,1);
         return ans;
+        
     }
 };
