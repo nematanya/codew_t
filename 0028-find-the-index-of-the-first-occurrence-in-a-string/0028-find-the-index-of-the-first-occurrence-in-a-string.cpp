91b6a5dba9b86f1e2d2270a6a0cd8d89f1f417ac
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2023-03-28 20:19:16 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (31.81%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int strStr(string h, string needle) {
+        // int n=needle.size();
+        // for(int i=0;i<h.size()-n;i++)
+        // {
+        //     if(h.substr)
+        // }
+        return h.find(needle);
+    }
+};
