70f8f8b154a240badd402730188746709c0d4f58
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2023-04-06 19:16:21 +0530

    Time: 239 ms (43.67%), Space: 108.9 MB (79.44%) - LeetHub

--- 2607-make-k-subarray-sums-equal/2607-make-k-subarray-sums-equal.cpp
+++ 2607-make-k-subarray-sums-equal/2607-make-k-subarray-sums-equal.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
  long long makeSubKSumEqual(vector<int>& arr, int k) {
-    long long sz = arr.size(), res = 0;
-    for (int i = 0; i < k; ++i) {
-        vector<int> cycle;
-        for (int j = i; arr[j] != 0; j = (j + k) % sz) {
-            cycle.push_back(arr[j]);
-            arr[j] = 0;
-        }
-        // nth_element(begin(cycle), begin(cycle) + cycle.size() / 2, end(cycle));
-sort(cycle.begin(),cycle.end());
-        for (int n : cycle)
-            res += abs(n - cycle[cycle.size() / 2]);
-    }
-    return res;
+ long long n=arr.size(),ans=0;
+     for(int i=0;i<k;++i)
+     {
+         vector<int>t;
+         for(int j=i;arr[j]!=0;j=(j+k)%n)
+         {
+             t.push_back(arr[j]);
+             arr[j]=0;
+         }
+      sort(t.begin(),t.end());
+      long long m;
+      for(auto it:t)
+          ans+=abs(it-t[(t.size()/2)]);
+     }
+     return ans;
 }
 };
