4437b1d9b0fc93c6d6a9374d5920d81f4dc97aae
Tanya Nema <44464895+nematanya@users.noreply.github.com>
2023-04-08 13:29:39 +0530

    Time: 160 ms (86.56%), Space: 49.9 MB (79.02%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     bool isPossibleDivide(vector<int>& nums, int k) {
-       unordered_map<int,int> mp;
-        sort(nums.begin(), nums.end());
-        if(nums.size()%k!=0){
-            return false;
-        }
-        for(int i=0; i<nums.size(); i++){
-            mp[nums[i]]++;
-        }
-        for(int i=0; i<nums.size(); i++){
-            if(mp[nums[i]]<=0){
-                continue;
-            }
-            for(int j=nums[i] ; j<nums[i]+ k; j++){
-                if(mp[j]<=0){
-                    return false;
-                }   
-                    mp[j]--;   
-            }
-        }
+        if(nums.size()%k!=0) return false;
+        sort(nums.begin(),nums.end());
+         unordered_map<int,int>mp;
+        for(auto it:nums)
+            mp[it]++;
+ for(int i=0;i<nums.size();i++)
+ {
+     if(mp[nums[i]]<=0) continue;
+     else 
+         for(int j=nums[i];j<nums[i]+k;j++)
+         {
+             if(mp[j]<=0) return false;
+             mp[j]--;
+         }
+ }
         return true;
+        
     }
 };
